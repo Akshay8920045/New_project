8df1cd6944b66e4f713ac2f800bbe2fe59a18141
Akshay123 <akshaysharma8920045@gmail.com>
2026-01-15 21:46:22 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import React, { useState }  from 'react'
-import Product from './product'
-import Nav from './nav'
+import React from 'react'
+import From from './form.jsx'
 
 
 function App() {
-  var [a, b] = useState(69);
 
   return (
-    <div>
-      
-   <h1 className='bg-amber-300'>Hi this is my second react project</h1>
-   <h2>{a+34}</h2>
-   <button onClick={() => b(a+34)} className='px-1 py-1 bg-amber-500'>Click me</button>
-   <nav>home</nav>
-   <div></div>
-   <Product naam  = "value"/>
-   <nav arr ={[34,"ghfh",67]}/>
-   </div>
-   
+    <From/>
   )
 }
 
---
+++ src/form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form><input placeholder='Enter the name' className='border-s-black p-2.5' />
+    <button className='border-s-amber-300 bg-amber-600' >Submit</button>
+    </form>
+
+  )
+}
+
+export default Form
