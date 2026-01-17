65ff072fca219d26b80c227d75b60bfc1abbe4fd
Akshay123 <akshaysharma8920045@gmail.com>
2026-01-17 21:44:09 +0530

    Update

--- src/form.jsx
+++ src/form.jsx
@@ -7,8 +7,10 @@ function Form() {
   };
   return (
     <form><input placeholder='Enter the name' className='border-s-black p-2.5'  value={name} onChange={handleChange}/>
+    <input type="email" name="Email" id="email" placeholder='Enter your gmail' />
     <button className='border-s-amber-300 bg-amber-600' >Submit</button>
     </form>
+   
 
   )
 }
